a91741f8e44fb407ffa3d8fd9c400cee790082d1
TAksenov <taksenov@gmail.com>
2017-11-13 17:48:40 +0500

    add spport for .editorconfig and set prettier for vscode

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import NewsPost from './NewsPost';
 
 import './App.css';
@@ -9,56 +9,52 @@ let id = 0;
 
 //CommonFunctions
 function getNewsId() {
-  id += 1;
-  return id;
+    id += 1;
+    return id;
 }
 //CommonFunctions
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            news: [],
+            newsInput: ''
+        };
 
-  constructor(props) {
-    super(props);
-    this.state = {
-        news: [],
-        newsInput: '', 
-      };
-    
-    // This binding is necessary to make `this` work in the callback
-    this.handleChange = this.handleChange.bind(this);  
-    this.handleKeyDown = this.handleKeyDown.bind(this);  
-  } //constructor
+        // This binding is necessary to make `this` work in the callback
+        this.handleChange = this.handleChange.bind(this);
+        this.handleKeyDown = this.handleKeyDown.bind(this);
+    } //constructor
 
-  handleChange(e) {
-    this.setState({newsInput: e.target.value});
-  } //handleChange
+    handleChange(e) {
+        this.setState({ newsInput: e.target.value });
+    } //handleChange
 
-  handleKeyDown(e) {
-    if (e.keyCode === 13) {
-      const { news, newsInput } = this.state;
-      const newNews = {id: getNewsId(), text: newsInput};
+    handleKeyDown(e) {
+        if (e.keyCode === 13) {
+            const { news, newsInput } = this.state;
+            const newNews = { id: getNewsId(), text: newsInput };
 
-      this.setState({ newsInput: '', news: [...news, newNews] });
+            this.setState({ newsInput: '', news: [...news, newNews] });
+        }
+    } //handleKeyDown
+
+    render() {
+        return (
+            <div className="App">
+                <input
+                    className="todo-input"
+                    type="text"
+                    placeholder="Add news"
+                    onChange={this.handleChange}
+                    onKeyDown={this.handleKeyDown}
+                    value={this.state.newsInput}
+                />
+                {this.state.news.map(newsPost => <NewsPost key={newsPost.id} text={newsPost.text} />)}
+            </div>
+        );
     }
-  } //handleKeyDown
-
-  render() {
-    return (
-      <div className="App">
-        <input
-          className='todo-input'
-          type="text" 
-          placeholder="Add news" 
-          onChange={this.handleChange} 
-          onKeyDown={this.handleKeyDown} 
-          value={this.state.newsInput} 
-        />
-        {this.state.news.map(newsPost => (
-          <NewsPost key={newsPost.id} text={newsPost.text} />
-        ))}
-      </div>
-    );
-  }
-
 } //App
 
 export default App;
